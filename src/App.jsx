aa4ec6d9112993c30e46b133126803831c953df5
Shaikh Sajed <85938068+myself-sajed@users.noreply.github.com>
2021-09-07 17:45:32 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 style={{ "text-align": "center" }}>React app started Successfully!</h1>
+    </>
+  );
+}
+
+export default App;
